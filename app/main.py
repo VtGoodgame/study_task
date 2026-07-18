0575824294537be0edcb7f0a24f3926efd75a5de
VtGoodgame <devisheva.ina@gmail.com>
2026-07-18 22:05:17 +0400

    finish commit

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ import uvicorn
 from fastapi import FastAPI
 from consts import api_version
 import routers
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 app = FastAPI(title="Study Task API", version=api_version)
 
@@ -10,4 +13,5 @@ app.include_router(routers.version.router)
 app.include_router(routers.health.router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9090)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    logger.info(f"API is running on http://localhost:8080 with version {api_version}")
