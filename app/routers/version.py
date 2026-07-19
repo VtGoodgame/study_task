994152d5a34326c048db2852a5656c90dccc2d76
VtGoodgame <devisheva.ina@gmail.com>
2026-07-19 18:34:08 +0400

    Check new branch

--- app/routers/version.py
+++ app/routers/version.py
@@ -1,13 +1,12 @@
 from fastapi import APIRouter, status
-from consts import api_version
+from consts import VERSION
 
-router = APIRouter(
-    prefix="/versions",            
+router = APIRouter( 
     tags=["versions"]
 )
 
 @router.get("/getversion", status_code=status.HTTP_200_OK,
            summary="Получить актуальную версию API")
 async def get_version():
-    return {"version": api_version,
+    return {"version": VERSION,
             "version_info": "Version details"}
